22e9e13588dffbea3550c2d3dbeffdfb34d8e980
Ariel Matsui-LeRoy <arielmatsuileroy@gmail.com>
2018-08-28 15:13:09 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { 'Travis Scott': 'COFFEE BEAN'}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = 'songTitle';
+  playlist.artistName = songTitle;
   return playlist;
 }
